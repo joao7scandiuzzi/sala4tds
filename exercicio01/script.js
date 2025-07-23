7ecb46b30930bed000d77a6b6a3cb02544c820cf
Joao Scandiuzzi <joaohilbigscandiuzzi@gmail.com>
2025-07-23 10:24:56 -0300

    Add files via upload

---
+++ exercicio01/script.js
@@ -0,0 +1,15 @@
+function soma(){
+    var numero1 = document.getElementById("n1").valueAsNumber
+    var numero2 = document.getElementById("n2").valueAsNumber
+    // var resposta = document.getElementById('resposta')
+    // resposta.textContent = numero1 + numero2
+    document.getElementById('resposta').textContent = numero1 + numero2
+}
+
+function sub(){
+    var numero1 = document.getElementById("n7").valueAsNumber
+    var numero2 = document.getElementById("n8").valueAsNumber
+    // var resposta = document.getElementById('resposta')
+    // resposta.textContent = numero1 + numero2
+    document.getElementById('respSub').textContent = numero1 - numero2
+}
